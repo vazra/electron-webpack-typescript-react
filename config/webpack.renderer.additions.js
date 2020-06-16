75f14f3af37e41b8cb2afb4215bf70209038716f
Ameen Rashad <ameenrashadv@gmail.com>
2020-06-16 20:38:04 +0530

    added csp webpack plugin

---
+++ config/webpack.renderer.additions.js
@@ -0,0 +1,13 @@
+const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+
+module.exports = {
+  // plugins: [
+  //   new CspHtmlWebpackPlugin({
+  //     "base-uri": "'self'",
+  //     "object-src": "'none'",
+  //     "default-src": ["'unsafe-inline'", "'self'", "'unsafe-eval'"],
+  //     "script-src": ["'self'", "'unsafe-eval'"],
+  //     "img-src": ["'self'", "data:"],
+  //   }),
+  // ],
+};
